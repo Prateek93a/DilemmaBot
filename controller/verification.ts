01158455526b37976b0cc9fb83f6da9f74e45264
Prateek Jain <prateek93a@gmail.com>
2021-07-27 15:15:13 +0530

    Update verification.ts

--- controller/verification.ts
+++ controller/verification.ts
@@ -3,12 +3,8 @@ export default (req,res) => {
 	const hubMode = req.query[`hub.mode`];
 	const verifyTokenMatches = (req.query[`hub.verify_token`] === process.env.VERIFICATION_TOKEN);
 	if(hubMode && verifyTokenMatches){
-		console.log('hello')
 		res.status(200).send(hubChallenge);
 	}else{
-		console.log('eroror')
 		res.status(403).end();
 	}
-	console.log('erererere')
-	return;
 };
